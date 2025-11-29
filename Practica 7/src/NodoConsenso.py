aa70e472e20f563c537437c1e36965acfdbab12d
GaboRivMa <gabrielrivema@ciencias.unam.mx>
2025-11-29 14:17:10 -0600

    Subiendo codigos

--- Practica 7/src/NodoConsenso.py
+++ Practica 7/src/NodoConsenso.py
@@ -17,7 +17,7 @@ class NodoConsenso(Nodo):
         # Atributos extra
         self.V = [None] * (len(vecinos) + 1) # Llenamos la lista de Nodos
         self.V[id_nodo] = id_nodo
-        self.New = set([id_nodo])
+        self.New = set([(id_nodo, id_nodo)])
         self.rec_from = [None] * (len(vecinos) + 1)
         self.fallare = False      # Colocaremos esta en True si el nodo fallará
         self.lider = None         # La elección del lider.
@@ -25,3 +25,49 @@ class NodoConsenso(Nodo):
     def consenso(self, env, f):
         '''El algoritmo de consenso.'''
         # Aquí va su implementación
+        if self.id_nodo < f:
+            self.fallare = True
+        
+        for ronda in range(1, f+2):
+            if self.fallare:
+                yield env.timeout(TICK)
+                continue
+
+            if len(self.New) > 0:
+                for j in self.vecinos:
+                    self.canal_salida.envia((self.id_nodo, self.New.copy()), [j])
+
+            yield env.timeout(TICK)
+
+            for j in range(len(self.rec_from)):
+                self.rec_from[j] = set()
+
+            mensajes_recibidos = []
+
+            while len(self.canal_entrada.items) > 0:
+                try:
+                    mensaje = yield self.canal_entrada.get()
+                    mensajes_recibidos.append(mensaje)
+                except simpy.Interrupt:
+                    break
+
+            for mensaje in mensajes_recibidos:
+                id_remitente, mensajes = mensaje
+                if id_remitente < len(self.rec_from):
+                    self.rec_from[id_remitente] = mensajes.copy()
+
+            self.New = set()
+            for j in self.vecinos:
+                for (v,k) in self.rec_from[j]:
+                    if k < len(self.V) and self.V[k] is None:
+                        self.V[k] = v
+                        self.New.add((v,k))
+            yield env.timeout(TICK)
+
+            v_decision = None
+            for valor in self.V:
+                if valor is not None:
+                    v_decision = valor
+                    break
+            self.lider = v_decision
+            return self.lider
